f7a7d2e5cc6d6a2438c1fbe9fb4d90f05de401df
Mustafa Dwaikat <mudwekat@gmail.com>
2025-11-14 22:14:50 +0300

    chore: initial project setup with tree-shakable configuration

---
+++ tsup.config.ts
@@ -0,0 +1,36 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig([
+  {
+    entry: {
+      index: 'src/index.ts',
+      json: 'src/json.ts',
+      yaml: 'src/yaml.ts',
+      toml: 'src/toml.ts',
+    },
+    format: ['esm', 'cjs'],
+    dts: true,
+    splitting: false,
+    sourcemap: true,
+    clean: true,
+    treeshake: true,
+    minify: false,
+    outDir: 'dist',
+  },
+  {
+    entry: {
+      cli: 'cli/index.ts',
+    },
+    format: ['esm'],
+    dts: false,
+    splitting: false,
+    sourcemap: true,
+    clean: false,
+    treeshake: true,
+    outDir: 'dist',
+    banner: {
+      js: '#!/usr/bin/env node',
+    },
+  },
+]);
+
